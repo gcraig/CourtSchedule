9e6dbbc80a0a5cb0bc454ce6f0ebc97f38af1450
George Craig <georgeacraig@gmail.com>
2011-05-31 00:33:25 -0500

    Update parse and insert function

--- CourtSchedule.py
+++ CourtSchedule.py
@@ -8,20 +8,16 @@ import glob
 import time
 import urllib
 
-# A simple script to import court schedule data from Will County, Illinois.
-# Fetches data from web, then saves it in an ETL table
+# A simple ETL script to import court schedule data from Will County, Illinois.
 # (c)2011. George Craig
 
 import_path = "import/"
 db_path = "db/schedule.db"
 
 datafile_url = "http://www.willcountycircuitcourt.com/schedule/PUBLIC/%s.txt"
+datafile_insert = "insert into schedule (schedule_name, schedule_date, schedule_room, schedule_time, schedule_case) values (?,?,?,?,?)"
 datafile_spec = "*.txt"
-datafile_fields = (0,19), (20,29), (30,37), (38,45), (46,61)
-datafile_format = None, formatdate, None, formattime, None
-
-x = ((20, 29),
-    ((30, 39), formatedate))
+datafile_sql = "select count(schedule_name) from schedule"
 
 def parsedate(ln, dateptn):
     match = dateptn.search(ln[20:29])
@@ -33,35 +29,49 @@ def fetchdata():
     for filename in xrange(ord('A'), ord('Z')+1):
         url = datafile_url % chr(filename)
         idx = url.rfind('/')
-        datafile = url[idx+1:]
+        datafile = import_path + url[idx+1:]
         print url, "->", datafile
-        if not os.path.exists(import_path + datafile):
+        if not os.path.exists(datafile):
             urllib.urlretrieve(url, datafile, fetchstatus)
 
 def importdata():
     print "importing data..."
-
     titleptn = re.compile(" NAME\s*DATE.*")
-    dateptn = re.compile(r'(\d{1,2})\s*(\d{1,2})\s*(\d{2})')
+    totctr = 0
 
-    conn = sqlite3.connect(db_path)
-    for datafile in glob.glob(os.path.join(import_path, datafile_spec)):
-        print "reading and inserting: " + datafile
-        f = open(datafile, 'r')
-        for ln in f:
-            if titleptn.match() or not ln.strip():
-                continue
-            else:
-                name = ln[0 :19].strip()
-                date = parsedate(ln, position, dateptn)
-                room = ln[30:37].strip()
-                hrs = ln[38:45].strip()
-                ctime = hrs[0:-2] + ":" + hrs[-2:]
-                ccase = ln[46:61].strip()
-                parms = (cname, cdate, croom, ctime, ccase)
-                cur = conn.cursor()
-                cur.execute("insert into schedule (name, datetime, room, case) values (?,?,?,?)", parms)
-        f.close()
+    try:
+        conn = sqlite3.connect(db_path)
+        for datafile in glob.glob(os.path.join(import_path, datafile_spec)):
+            print "reading %s" % datafile
+            f = open(datafile, 'r')
+            ctr = 0
+            for ln in f:
+                if not ln.strip() or titleptn.match(ln):
+                    continue
+                else:
+                    ctr += 1
+                    _name = ln[0:19].strip()
+                    _date = "".join((ln[20:22].strip(), "/", ln[23:25].strip(), "/20", ln[27:29].strip()))
+                    _room = ln[30:37].strip()
+                    _time = ln[38:45].strip()
+                    _time = _time[0:-2] + ":" + _time[-2:]
+                    _case = ln[46:61].strip()
+                    parms = (_name, _date, _room, _time, _case)
+                    cur = conn.cursor()
+                    cur.execute(datafile_insert, parms)
+            f.close()
+            totctr += ctr
+            print "read and insert: %d recs" % ctr
+            sys.stdout.flush()
+
+        cur = conn.cursor()
+        cur.execute(datafile_sql)
+        for row in cur:
+            print  "total records in db: %d" % row[0]
+        print "total records read from file: %d" % totctr
+    finally:
+        conn.commit()
+        conn.close()
 
 def dumpdata():
     print "dumping data..."
@@ -77,4 +87,5 @@ if __name__ == "__main__":
     fetchdata()
     importdata()
     end = time.clock()
-    print end-start
+    elapsed = end - start
+    print "%.2f secs" % elapsed
